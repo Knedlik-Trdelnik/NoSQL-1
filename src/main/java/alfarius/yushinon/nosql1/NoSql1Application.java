14d12b2b2e799f27fbbbcf9cc11843e3efd5c564
Никита <podberezkin.n@mail.ru>
1999-09-20 19:21:30 +0300

    start

---
+++ src/main/java/alfarius/yushinon/nosql1/NoSql1Application.java
@@ -0,0 +1,13 @@
+package alfarius.yushinon.nosql1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoSql1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoSql1Application.class, args);
+    }
+
+}
---
+++ src/test/java/alfarius/yushinon/nosql1/NoSql1ApplicationTests.java
@@ -0,0 +1,13 @@
+package alfarius.yushinon.nosql1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoSql1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
