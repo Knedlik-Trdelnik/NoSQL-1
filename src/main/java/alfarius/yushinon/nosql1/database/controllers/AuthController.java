ed72d3a5615c4e08dec53c75255c51a5af049d7f
Никита <podberezkin.n@mail.ru>
1999-09-21 04:40:16 +0300

    add new libs

--- src/main/java/alfarius/yushinon/nosql1/database/controllers/AuthController.java
+++ src/main/java/alfarius/yushinon/nosql1/database/controllers/AuthController.java
@@ -1,19 +1,33 @@
 package alfarius.yushinon.nosql1.database.controllers;
 
+import alfarius.yushinon.nosql1.database.services.AuthService;
 import alfarius.yushinon.nosql1.entity.User;
+import alfarius.yushinon.nosql1.util.AuthResponse;
+import alfarius.yushinon.nosql1.util.RegisterRequest;
+import alfarius.yushinon.nosql1.util.LoginRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//https://habr.com/ru/articles/1059086/
+
 @RestController
 @RequestMapping("/api/auth")
+@RequiredArgsConstructor
 public class AuthController {
 
-    @PostMapping
-    public User createUser(@RequestBody User user) {
-        return new User();
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request){
+        return ResponseEntity.ok(authService.register(request));
     }
 
-
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login (@RequestBody LoginRequest request){
+        return ResponseEntity.ok(authService.login(request));
+    }
 }
--- src/main/java/alfarius/yushinon/nosql1/entity/User.java
+++ src/main/java/alfarius/yushinon/nosql1/entity/User.java
@@ -3,6 +3,7 @@ package alfarius.yushinon.nosql1.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
 @Table(name = "users")
